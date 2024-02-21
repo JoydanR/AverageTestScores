a12007e83177739b7b84412f3a7fd49bcb3fffca
JoydanR <68913366+JoydanR@users.noreply.github.com>
2024-02-21 14:59:16 -0500

    Add files via upload

---
+++ AverageTestScores.java
@@ -0,0 +1,27 @@
+//Joy'dan Rowley
+//Create a program that will calculate the average of 5 test scores.
+
+
+
+\
+public class AverageTestScores{
+
+   public static double calcAverage(double score1, double score2, double score3, double score4, double score5 ) {
+      double average; 
+      average = ( score1 + score2 + score3 + score4 + score5 ) / 5; 
+      return average;
+   }
+
+   public static totalGrade( double testScore ) {
+    
+    
+    
+    }
+         
+
+   public static void main( String [ ] args ) {
+            
+      
+    }
+}
+
